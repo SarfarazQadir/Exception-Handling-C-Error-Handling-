6402d56e354d665c41db4e787a0da1d57b80d67b
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-06-05 06:09:44 +0500

    Add files via upload

---
+++ Exception Handling (Error Handling)/Exception Handling (Error Handling)/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling__Error_Handling_
+{
+    internal class Exception
+    {
+
+        public void show()
+        {
+          /*  Console.WriteLine("Enter a number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = 0;
+
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }*/
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Exception obj = new Exception();
+             obj.show();
+             Console.WriteLine("Your result is : {0}", obj.show());*/
+
+            Console.WriteLine("Enter a number");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = 0;
+
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Your Result {0}",result);
+            Console.ReadLine();
+        }
+    }
+}
